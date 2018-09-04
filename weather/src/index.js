baa929f6cfe4651c27c97aea6cc2848a666d8b19
bear-dog-lover <lagrange.resolvent@gmail.com>
2018-09-04 22:58:40 +0900

    Revert "一部変数名を変更＋改善案２を実装"
    This reverts commit d0523b67950f28ac96e32e023a47771cc6fa5a15.

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,137 +1,95 @@
-function Square(props) {
-  return (
-    <button className="square" onClick={() => props.onClick()}>
-      {props.value}
-    </button>
-  );
-}
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
+import { List, ListItem } from 'material-ui/List'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
+import RefreshIndicator from 'material-ui/RefreshIndicator'
+import WeatherIcon from 'material-ui/svg-icons/image/wb-sunny'
+import TemperatureIcon from 'material-ui/svg-icons/editor/show-chart'
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+class WeatherPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {placeName: null, weather: null, temperature: null, loading: false}
+    
+    this.Places = [{name: '札幌', id: 2128295}, {name: '秋田', id: 2113126}, {name: '東京', id: 1850147},
+                   {name: '大阪', id: 1853909}, {name: '沖縄', id: 1894616}]
+    this.OpenWeatherMapKey = "175743ae3d18573654d028e0a163497c"
+  }
+  selectPlace(index) {
+    if (index > 0) {
+      const place = this.Places[index - 1]
+      this.setState({placeName: place.name, weather: null, temperature: null, loading: true})
+      this.getWeather(place.id)
+    }
+  }
+  getWeather(id) {
+    const delay = (mSec) => new Promise((resolve) => setTimeout(resolve, mSec))
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${this.OpenWeatherMapKey}&id=${id}&lang=ja&units=metric`)
+    .then((response) => response.json())
+    .then((json) => {
+      delay(700)
+      .then(() => this.setState({weather: json.weather[0].description,
+                                 temperature: json.main.temp, loading: false}))
+    })
+    .catch((response) => {
+      this.setState({loading: false})
+      console.log('** error **', response)
+    })
   }
   render() {
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      <MuiThemeProvider>
+        <Card style={{margin: 30}}>
+          <CardHeader title={<Title place={this.state.placeName} />} />
+          <CardText style={{position: 'relative'}}>
+            <RefreshIndicator status={this.state.loading ? 'loading' : 'hide'} top={40} left={100} loadingColor="#f00" />
+            <WeaterInfomation weather={this.state.weather} temperature={this.state.temperature} />
+          </CardText>
+          <CardActions>
+            <PlaceSelector places={this.Places} actionSelect={(ix) => this.selectPlace(ix)} />
+          </CardActions>
+        </Card>
+      </MuiThemeProvider>
+    )
   }
 }
 
-class Game extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-        posX: -1,
-        posY: -1,
-      }],
-      xIsNext: true,
-      stepNumber: 0,
-    };
-  }
-  handleClick(i) {
-    var history = this.state.history;
-    var current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-        posX: Math.floor(i / 3) + 1,
-        posY: i % 3 + 1
-      }]),
-      xIsNext: !this.state.xIsNext,
-      stepNumber: history.length,
-    });
-  }
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) ? false : true,
-    });
-  }
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    const moves = history.map((obj, step) => {
-      const desc = step ? 'Move (' + obj.posX + ',' + obj.posY + ')' :'Game start';
-      const bold = this.state.stepNumber == step ? {fontWeight: 'bold'} : {fontWeight: 'normal'};
-      return (
-        <li key={step} style={bold}>
-          <a href="#" onClick={() => this.jumpTo(step)}>{desc}</a>
-        </li>
-      );
-    });
-    
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
+
+const Title = (props) => (
+  <h1>{props.place ? props.place + 'の天気' : '天気情報'}</h1>
+)
+Title.propTypes = {
+  place: PropTypes.string
 }
 
-// ========================================
+const WeaterInfomation = (props) => (
+  <List>
+    <ListItem leftIcon={<WeatherIcon/>} primaryText={props.weather} />
+    <ListItem leftIcon={<TemperatureIcon />} primaryText={props.temperature ? `${props.temperature} ℃` : ''} />
+  </List>
+)
+WeaterInfomation.propTypes = {
+  weather: PropTypes.string,
+  temperature: PropTypes.number
+}
+
+const PlaceSelector = (props) => (
+  <DropDownMenu value={-1} onChange={(event, index) => props.actionSelect(index)}>
+    <MenuItem value={-1} primaryText="場所を選択" />
+    {props.places.map((place, ix) => <MenuItem key={ix} value={ix} primaryText={place.name} />)}
+  </DropDownMenu>
+)
+PlaceSelector.propTypes = {
+  places: PropTypes.array,
+  actionSelect: PropTypes.func
+}
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('container')
-);
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+  <WeatherPage />,
+  document.getElementById('root')
+)
