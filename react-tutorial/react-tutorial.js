da549247b7dbf562629bf90dd0dd3f2621e8b4ad
bear-dog-lover <lagrange.resolvent@gmail.com>
2018-09-05 12:00:39 +0900

    改善案５を実装！！！

--- react-tutorial/react-tutorial.js
+++ react-tutorial/react-tutorial.js
@@ -1,21 +1,29 @@
 function Square(props) {
+  let cname = "square";
+  if (props.bgColor) {
+    cname += " " + props.bgColor;
+  }
   return (
-    <button className="square" onClick={() => props.onClick()}>
+    <button className={cname} onClick={() => props.onClick()}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square key={i.toString()} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  renderSquare(i, bgColor) {
+    return <Square key={i.toString()} value={this.props.squares[i]} bgColor={bgColor} onClick={() => this.props.onClick(i)} />;
   }
   render() {
     const ret = [];
+    const straightLine = this.props.straightLine.slice();
     for (let i = 0; i < 3; i++) {
       let row = [];
       for (let j = 0; j < 3; j++) {
-        row.push(this.renderSquare(i * 3 + j));
+        row.push(this.renderSquare(
+          i * 3 + j, 
+          straightLine.indexOf(i * 3 + j) >= 0 ? "highlight" : ""
+        ));
       }
       ret.push(<div key={i.toString()} className="board-row">{row}</div>);
     }
@@ -39,13 +47,14 @@ class Game extends React.Component {
       xIsNext: true,
       stepNumber: 0,
       isAsc: true,
+      straightLine: [],
     };
   }
   handleClick(i) {
     var history = this.state.history;
     var current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (findStraightLine(squares).length > 0 || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -57,6 +66,7 @@ class Game extends React.Component {
       }]),
       xIsNext: !this.state.xIsNext,
       stepNumber: history.length,
+      straightLine: findStraightLine(squares),
     });
   }
   jumpTo(step) {
@@ -73,8 +83,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    
+    const winner = this.state.straightLine.length == 0 ? null : current.squares[this.state.straightLine[0]];
     let history_disp = history.slice();
     let order = "";
     if (!this.state.isAsc) {
@@ -85,7 +94,7 @@ class Game extends React.Component {
       const desc = obj.posX != -1 ? 'Move (' + obj.posX + ',' + obj.posY + ')' :'Game Start';
       const bold = this.state.stepNumber == step ? {fontWeight: 'bold'} : {fontWeight: 'normal'};
       return (
-        <li key={step} style={bold}>
+        <li key={step} style={bold} >
           <a href="#" onClick={() => this.jumpTo(step)}>{desc}</a>
         </li>
       );
@@ -97,13 +106,13 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-    
     return (
       <div className="game">
         <div className="game-board">
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            straightLine={this.state.straightLine}
           />
         </div>
         <div className="game-info">
@@ -126,7 +135,7 @@ ReactDOM.render(
   document.getElementById('container')
 );
 
-function calculateWinner(squares) {
+function findStraightLine(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -140,8 +149,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return lines[i]
     }
   }
-  return null;
+  return [];
 }
